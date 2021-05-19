802b94d44468f88ff0d6047230a4e2f27886593f
brainburster <brainburster@outlook.com>
2021-05-19 21:53:11 +0800

    修复编码问题

--- AST.h
+++ AST.h
@@ -10,6 +10,7 @@
 
 //#define L children[0]
 //#define R children[1]
+
 template<typename T>
 inline bool SafeDelete(T* p)
 {
@@ -24,7 +25,7 @@ namespace AST
 	struct AST
 	{
 		virtual ~AST() = default;
-		virtual void forEachChild(const std::function<void(AST * child)>&) {};
+		virtual void forEachChild(const std::function<void(AST* child)>&) {};
 	};
 
 	template<int n> struct Tree : AST
@@ -37,7 +38,7 @@ namespace AST
 				SafeDelete(children[i]);
 			}
 		}
-		void forEachChild(const std::function<void(AST * child)>& callback) override
+		void forEachChild(const std::function<void(AST* child)>& callback) override
 		{
 			for (int i = 0; i < n; ++i)
 			{
@@ -61,25 +62,30 @@ namespace AST
 		int type() override { return t; }
 	};
 
-	//2Ԫ����ʽ
+	//2元表达式节点
 	template<int t> struct BinExpr;
 	template<> struct BinExpr<NULL> { virtual void RTTI() {} };
 	template<int t = NULL> struct BinExpr : Tree<2>, ASTypeToStr<t>, BinExpr<NULL> {};
-	//����ʽ
+
+	//表达式节点
 	using Expr = BinExpr<>;
 
-	//��ʶ��
+	//变量名节点
 	struct ID : Tree<0>, Expr { int id = 0; };
 	using Name = ID;
-	//ֵ
+
+	//数字，字符串，bool节点
 	struct NumValue : Tree<0>, Expr { double value = 0; };
 	struct StrValue : Tree<0> { int id = 0; };
 	struct BoolValue : Tree<0>, Expr { bool value = false; };
+	//求反节点
 	struct Negative : Tree<1>, Expr {};
-	//ʵ�Σ��β��б�
+
+	//实参和形参节点
 	struct Args : Tree<2> {};
 	struct Params : Tree<2> {};
-	//���
+
+	//语句节点
 	struct Stats : Tree<2> {};
 	struct Echo : Tree<1> {};
 	struct FuncDef : Tree<3> {};
@@ -87,11 +93,14 @@ namespace AST
 	struct IF : Tree<3> {};
 	struct Else : Tree<1> {};
 
+	//获取左孩子
 	template<typename T, typename Enable = std::enable_if_t<1 <= std::extent_v<decltype(T::children)>>>
 	AST*& L(T* t)
 	{
 		return t->children[0];
 	}
+
+	//获取右孩子
 	template<typename T, typename Enable = std::enable_if_t<2 <= std::extent_v<decltype(T::children)>>>
 	AST*& R(T* t)
 	{
@@ -118,7 +127,7 @@ namespace AST
 	template<typename... Args>
 	constexpr int Length_v = Length<Args...>::value;
 
-	//��֮����
+	//创建语法树节点的工厂方法
 	template<typename T, typename... Args, typename Enable = std::enable_if_t<std::extent_v<decltype(T::children)> >= Length_v<Args...> && ((std::is_base_of_v<AST, std::remove_pointer_t<Args>> || std::is_same_v<nullptr_t, Args>) &&...) >>
 	inline auto Create(Args... args) ->T* //-> Tree<std::extent_v<decltype(T::children)>>*
 	{
@@ -160,14 +169,14 @@ namespace AST
 		return new T{};
 	}
 
-	//��Ԫ����������
+	//创建二元表达式节点
 	template<int type>
 	inline auto CreateBinExpr(AST* a, AST* b)
 	{
 		return Create<BinExpr<type>>(a, b);
 	};
 
-	//�Ѷ��ֽ��ַ�����������ת��Ϊ�ַ���
+	//打印节点类型
 	inline std::string ASTypeToStr<>::toString()
 	{
 		int tempi = type();
